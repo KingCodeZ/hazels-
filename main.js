918f22d60a29ae6f8b6cac7ba37a9c0c4dd13f20
KingCodeZ <160741812+KingCodeZ@users.noreply.github.com>
2025-02-14 20:41:50 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,60 @@
+var app = document.getElementById('app')
+
+var Typewriter = new Typewriter(app, {
+    loop: true,
+    delay : 55,
+});
+
+Typewriter
+    .pauseFor(2000)
+    .typeString('Hazelnut, ')
+    .pauseFor(300)
+    .deleteChars(20)
+    .typeString(" I find it difficult to write about you<br>")
+    // .deleteChars(161)
+    .pauseFor(4000)
+    .typeString(" It's odd, considering how much you inspire me<br> ")
+    .typeString(" How much i admire you <br> ")
+    .typeString(" How much i want you <br>      ")
+    .pauseFor(4000)
+
+    .typeString(" But it's, it's difficult to write about you <br> ")
+    .typeString(" There's something magic about you <br>")
+    .typeString("you remind me of art <br> ")
+    .typeString("a poem <br> ")
+    .typeString("a mysterious grimoire, beautiful carefully curated creative piece of words,  <br> ")
+    .typeString("But...   <br> ")
+    .pauseFor(4000)
+    .typeString("Those beautiful brown eyes,<br> ")
+    .typeString("The way your eyebrows sit in a curve when relaxed, <br> ")
+    .typeString("The outline and curve of your lips<br> ")
+    .typeString("And, God, the heartwarming smiles they give<br> ")
+    .typeString("Your neck, arms and fingers  <br> ")
+    .typeString("Chest and torso  <br> ")
+    .typeString("Waist and legs <br> ")
+    .typeString("I wish to say more but deceny permits me not . <br> ")
+    .typeString("Your little gestures, crazy laughs<br> ")
+    .typeString("Your choices, your voice<br> ")
+    .typeString("When our hands touch, your hugs<br> ")
+    .typeString("Your touch, your scent<br> ")
+    .typeString("The way you talk..<br> ")
+    .typeString("How i think of you when that part of the music plays <br> ")
+    .typeString("You're the fall and the rise of a musical number <br> ")
+    .typeString("The heart grasping climatic scene of a play<br> ")
+    .typeString("The brightly scattered stars on a dark night<br> ")
+    .typeString("The calm that makes my day<br> ")
+    .typeString("God knows I love beauty, creativity and art. <br> ")
+    .typeString("I guess that's why he sent you my way.<br> ")
+    .typeString("I hope to show you this one day<br> ")
+    .typeString("Or might tuck this one away in the imbre of my hidden abyss<br> ")
+    .typeString(' <h1 style="color: pink;">I like you hazelnut, way way more than friends should</h1> <br>')
+    .pauseFor(300)
+    .typeString(" <i>Love, kingsssss</i>")
+
+
+    .pauseFor(1000)
+    .start()
+
+
+
+
